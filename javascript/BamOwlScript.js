e0c6b09cda754544629d8c640e52e918506e53f6
Bamtoliya <31297221+Bamtoliya@users.noreply.github.com>
2020-07-24 23:48:55 -0400

    Update the CSS and Img

--- javascript/BamOwlScript.js
+++ javascript/BamOwlScript.js
@@ -1,21 +1,5 @@
 // JavaScript source code
 
-function dropdown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
 function openNav() {
     document.getElementById("mySidenav").style.width = "230px";
 }
