1327821f573b347d5813a020e3cfcd977faa1e4f
Bamtoliya <Bamtoliya@gmail.com>
2024-08-12 07:05:59 +0900

    Update notion-importer.js

--- _scripts/notion-importer.js
+++ _scripts/notion-importer.js
@@ -43,17 +43,18 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
 		console.log(r)
 		const id = r.id
 		// date
-		
-		let cdate = formatDate(r.created_time, "YYYY-MM-DD HH:MM")
+		let cdate = moment(r.created_time).format("YYYY-MM-DD HH:MM")
+		console.log(r.created_time)
+		console.log(cdate);
 		let pcdate = r.properties?.['Date']?.['date']?.['start']
 		if (pcdate) {
-			cdate = pdate.format('YYYY-MM-DD HH:MM')
+			cdate = moment(pdate).format('YYYY-MM-DD HH:MM')
 		}
 
-		let edate = r.last_edited_time.format("YYYY-MM-DD HH:MM")
+		let edate = moment(r.last_edited_time).format("YYYY-MM-DD HH:MM")
 		let pedate = r.properties?.['Date']?.['date']?.['start']
 		if (pedate) {
-			edate = edate.format('YYYY-MM-DD HH:MM')
+			edate = moment(edate).format('YYYY-MM-DD HH:MM')
 		}
 		// title
 		let title = id
