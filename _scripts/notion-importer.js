2ad647a5cd17f259a8b228701f9f69ae9f70e78d
Bamtoliya <Bamtoliya@gmail.com>
2024-08-12 04:12:02 +0900

    Update notion-importer.js

--- _scripts/notion-importer.js
+++ _scripts/notion-importer.js
@@ -30,17 +30,22 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
 					checkbox: {
 						equals: true
 					}
-				},
-				{
-					"or" : [
-						{
-							timestamp: "created_time",
-							"created_time": {
-								on_or_after: lastUpdate
-							}
-						}
-					]
+				},{
+					timestamp: "last_edited_time",
+					"last_edited_time": {
+						on_or_after: lastUpdate
+					}
 				}
+				// {
+				// 	"or" : [
+				// 		{
+				// 			timestamp: "created_time",
+				// 			"created_time": {
+				// 				on_or_after: lastUpdate
+				// 			}
+				// 		}
+				// 	]
+				// }
 			]			
 		}
 	})
